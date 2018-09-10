d1fa75e982e041e73b80af3d57fd1dd473b87053
tylergreulich <tgreul6@gmail.com>
2018-09-10 15:35:48 -0700

    fixed jest script

--- src/utils/searchOriginalError.ts
+++ src/utils/searchOriginalError.ts
@@ -2,9 +2,11 @@ const searchOriginalError = error => {
   if (error.originalError) {
     return searchOriginalError(error.originalError);
   }
+
   if (error.errors) {
     return error.errors.map(searchOriginalError)[0];
   }
+
   return error;
 };
 
